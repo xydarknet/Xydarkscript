650bce744dea0cbe4c08967fdac7906ca4ef2bb9
xydarknet <119782134+xydarknet@users.noreply.github.com>
2025-07-25 00:22:15 +0800

    Create ceklog.sh

---
+++ ceklog.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Cek login aktif VLESS & Trojan WS + gRPC
+
+echo "Login aktif akan tampil di sini (future enhancement bisa pakai log parsing atau API)"
+read -rp "Tekan Enter untuk kembali..."
+bash /usr/bin/xray/menu-xray.sh
